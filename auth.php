03283f9614d54cde7d9c6d58477c91732cb71e2b
telecomsxchange <tier1@telecomsxchange.com>
2017-03-27 06:33:27 +0200

    Update auth.php
    Removing extra php tag

--- auth.php
+++ auth.php
@@ -1,11 +1,15 @@
-<?php
+
 
 // AUTHENTICATION CREDINTIALS FOR TELECOMSXCHANGE
 
 $tcxc_host = "https://members.telecomsxchange.com";
+                                  
 //TCXC (TelecomsXChange) Buyer login
 $login = "{{ Buyer_Username }}";
+
+
 //TCXC (TelecomsXChange) API key, Generated from Buyer Portal, Preferences Page:
 $api_key = "{{ API_KEY }}";
+
 //TCXC i_account that you want to use for Callback billing, 
 $i_account = 1; // REPLACE 1 with your own values
