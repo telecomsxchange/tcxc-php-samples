e0d033a08a7e39e4f59eb1e2ac83ca33c79b7a5c
TelecomsXChange <tier1@telecomsxchange.com>
2017-11-14 23:47:23 +0200

    Update call-viber-number-sample.php
    Cleanup code, add some comments and fixes

--- call-viber-number-sample.php
+++ call-viber-number-sample.php
@@ -2,22 +2,40 @@
 
 <?php
 
-// THE NUMBERS YOUR DIALINH MUST BE ONLINE WITH THE VIBER APP/network for it ro work
+// The number your dialing must be online with Viber in order to be able to reach them.
 
+
+
+
+// Include API key and other required authentication data
 include 'auth.php';
-include 'carrier.php';  // import carrier i_connection ids by name
-//leg A parameters
-$cld1 = "19542405555";  //First number to dial
+
+
+// import carrier i_connection ids by name
+include 'carrier.php';  
+
+
+
+// leg A parameters
+$cld1 = "19542401111";  //First number to dial
 $cli1 = "1800999999";  // Caller ID to show first number
 $i_connection1 = $VIBER;  // Carrier to use is viber Communications
-//leg B parameters
-$cld2 = "18667471117"; //Second number to dial
+
+
+// leg B parameters
+
+$cld2 = "18667471117"; //Second number to dial- The number subscribed to Viber
 $cli2 = "12345678988"; //Caller ID t show destination number
 $i_connection2 = $VIBER;  // Carrier to use is viber
 $ts = time();   // Get Current Time Stamp
 $check_uri = "/api/callback/initiate/$login/$i_account/$cld1/$cli1/$i_connection1/$cld2/$cli2/$i_connection2/$ts/";
+
+
+
 // Signing the API key for better security
 $sign = hash('sha256',$check_uri . $api_key);
+
+
 // initiate the call from your script
 $handle=curl_init( $tcxc_host . $check_uri . $sign );
 curl_setopt($handle, CURLOPT_VERBOSE, true);
