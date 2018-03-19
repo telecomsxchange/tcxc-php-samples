02d8ee45e820d7c929928f7f5348d03a68f87f1d
TelecomsXChange <tier1@telecomsxchange.com>
2018-03-19 02:02:56 +0200

    Create Make_outbound_call_via_TelecomsXChange_CLI.php

---
+++ Make_outbound_call_via_TelecomsXChange_CLI.php
@@ -0,0 +1,48 @@
+<?php
+
+$tcxc_host = "https://members.telecomsxchange.com";
+
+//TCXC (TelecomsXChange) Buyer login
+$login = "{{ Buyer_Username }}";
+
+//TCXC (TelecomsXChange) API key, Generated from Buyer Portal, Preferences Page:
+
+$api_key = "{{ API_KEY }}";
+
+//TCXC i_account that you want to use for Callback billing, 
+
+
+$i_account = 1;  // You can get account ID from Accounts Page
+
+
+//legA parameters
+
+$cld1 = "19542405555";  //First number to dial
+$cli1 = "1800999999";  // Caller ID to show first number
+$i_connection1 = 785;  // Using TelecomsXChange CLI Route to connect
+
+//legB parameters
+$cld2 = "18667478647"; //Second number to dial
+$cli2 = "19542405412"; //Caller ID t show destination number
+$i_connection2 = 785;  // Using TelecomsXChange CLI Route to connect
+
+$ts = time();   // Get Current Time Stamp
+
+$check_uri = "/api/callback/initiate/$login/$i_account/$cld1/$cli1/$i_connection1/$cld2/$cli2/$i_connection2/$ts/";
+
+// Signing the API key for better security
+
+$sign = hash('sha256',$check_uri . $api_key);
+
+// initiate the call from your script
+
+$handle=curl_init( $tcxc_host . $check_uri . $sign );
+
+curl_setopt($handle, CURLOPT_VERBOSE, true);
+curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($handle, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($handle, CURLOPT_SSL_VERIFYHOST, false);
+
+$content = curl_exec($handle);
+
+echo $content;
